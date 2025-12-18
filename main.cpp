2506a1cb040be3b3e08a9eb9072cdc4728b00f75
IbraheemSultan <ebrehem47@gmail.com>
2025-12-18 20:57:30 +0200

    remove copy project

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "clientwindow.h"
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    ClientWindow w;
-    w.show();
-    return a.exec();
-}
